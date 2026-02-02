b8b2e7a016e9fa772b94a2f97407e7a6a6af80c1
Nora <starforgecon@gmail.com>
2026-02-02 11:46:27 -0800

    Initial commit

---
+++ src/main/java/nora/truckstop/NorasTruckStopClient.java
@@ -0,0 +1,10 @@
+package nora.truckstop;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NorasTruckStopClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/nora/truckstop/NorasTruckStopDataGenerator.java
@@ -0,0 +1,11 @@
+package nora.truckstop;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NorasTruckStopDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
